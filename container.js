ec87275150315d17f637c04db26af746769e8a95
Sangyoun Kim <s265kim@uwaterloo.ca>
2018-03-06 14:11:37 -0500

    Update getContianers to provide drug info

--- container.js
+++ container.js
@@ -1,8 +1,33 @@
-function Container(data) {
-    this.id = data.id;
-    this.pill_type = data.pill_type;
-    this.reminder_time = data.reminder_time;
-    this.frequency = data.frequency;
+class Container {
+    constructor({
+        id,
+        pill_type,
+        reminder_time,
+        frequency,
+        last_opened,
+        brand_name: brandName,
+        manufacturer_name: manufacturerName,
+        substance_name: substanceName,
+        purpose,
+        indications_and_usage: indicationsAndUsage,
+        brand_primary_color: brandPrimaryColor,
+        brand_secondary_color: brandSecondaryColor,
+    }) {
+        this.id = id;
+        this.pillType = pill_type;
+        this.reminderTime = reminder_time;
+        this.frequency = frequency;
+        this.lastOpened = last_opened;
+        this.drugInfo = {
+            brandName,
+            manufacturerName,
+            purpose,
+            substanceName,
+            indicationsAndUsage,
+            brandPrimaryColor,
+            brandSecondaryColor,
+        };
+    }
 }
 
 module.exports = Container;
